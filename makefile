8334c1a09638ee3e72f7c65323811ed6db62f7a5
tmadlener <thomas.madlener@cern.ch>
2020-03-23 19:02:14 +0100

    Use ArgParser from chi pol fw to get clargs

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ ROOTLIBS=$(shell root-config --libs) -lMinuit2
 CXXFLAGS=-std=c++14 -Wall -Wextra -Wpedantic -Wshadow -O3
 CXX=g++
 
-INCS=-I$(shell root-config --incdir) -I./interface
+INCS=-I$(shell root-config --incdir) -I./interface -I$(CHIB_CHIC_POLFW_DIR)/general/interface
 
 BIN=./bin/
 SRC=./src/
--- src/run_fit.cc
+++ src/run_fit.cc
@@ -1,9 +1,16 @@
 #include "global_fit.C"
+#include "ArgParser.h"
+
 
 #if !(defined(__CINT__) or defined(__CLING__))
-int main(int, char**)
+int main(int argc, char* argv[])
 {
-  global_fit();
+  const auto parser = ArgParser(argc, argv);
+  const auto nScanPoints = parser.getOptionVal<unsigned>("--nscan", 5000000);
+  const auto outfile = parser.getOptionVal<std::string>("--outfile", "results/scan_file.root");
+
+
+  global_fit(nScanPoints, outfile);
   return 0;
 }
 #endif
