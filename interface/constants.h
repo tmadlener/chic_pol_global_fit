c0275c041a0c215a0b58089e12e20bc0b1aea4e2
tmadlener <thomas.madlener@cern.ch>
2020-04-02 16:18:37 +0200

    Updated branching fractions to 2020 PDG values

--- interface/constants.h
+++ interface/constants.h
@@ -3,14 +3,14 @@
 
 #include <array>
 
-// Some PDG branching fractions and their uncertainties from the 2015 PDG
-constexpr std::array<double, 2> B_PSIP_MM = {7.9e-3, 0.9e-3};
-constexpr std::array<double, 2> B_PSIP_CHIC2 = {9.11e-2, 0.31e-2};
-constexpr std::array<double, 2> B_PSIP_CHIC1 = {9.55e-2, 0.31e-2};
-constexpr std::array<double, 2> B_PSIP_JPSI = {60.9e-2, 0.6e-2};
-constexpr std::array<double, 2> B_PSIP_PIPI = {34.46e-2, 0.3e-2};
-constexpr std::array<double, 2> B_CHIC2_JPSI = {19.2e-2, 0.7e-2};
-constexpr std::array<double, 2> B_CHIC1_JPSI = {33.9e-2, 1.2e-2};
+// Some PDG branching fractions and their uncertainties from the 2020 PDG
+constexpr std::array<double, 2> B_PSIP_MM = {8.0e-3, 0.6e-3};
+constexpr std::array<double, 2> B_PSIP_CHIC2 = {9.52e-2, 0.20e-2};
+constexpr std::array<double, 2> B_PSIP_CHIC1 = {9.75e-2, 0.24e-2};
+constexpr std::array<double, 2> B_PSIP_JPSI = {61.4e-2, 0.6e-2};
+constexpr std::array<double, 2> B_PSIP_PIPI = {34.68-2, 0.3e-2};
+constexpr std::array<double, 2> B_CHIC2_JPSI = {19.0e-2, 0.5e-2};
+constexpr std::array<double, 2> B_CHIC1_JPSI = {34.3e-2, 1.0e-2};
 constexpr std::array<double, 2> B_JPSI_MM = {5.961e-2, 0.033e-2};
 
 // masses in GeV
