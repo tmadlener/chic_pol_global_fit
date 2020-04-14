87d92c6548ca6a837f2652714056169ff57ada75
tmadlener <thomas.madlener@cern.ch>
2020-04-14 16:13:04 +0200

    Add script to obtain band graphs as a function of pT/M

--- python/common_helpers.py
+++ python/common_helpers.py
@@ -3,6 +3,9 @@
 Module containing some common helper functionality
 """
 
+import numpy as np
+
+
 def frac_to_lam(frac):
     """Transform a longitudinal fraction into a lambda value"""
     return (1 - 3 * frac) / (1 + frac)
@@ -18,3 +21,26 @@ def get_var_name(var, trans):
     if trans == 'identity':
         return var
     return var.replace('f_long_c', 'lambda_')
+
+
+def get_variable_binning(var_vals, get_vals=False):
+    """Get a binning that places every data point into a separate bin. If get_vals
+    is True, also return the unique values"""
+    # Get the unique values, and split the intervals between them by half to
+    # get the binning. For the first and last bin reuse the intervals of the
+    # adjacent bins
+    vals = np.sort(np.unique(var_vals))
+    diffs = np.diff(vals)
+
+    diffs = np.append(np.array([diffs[0]]), diffs)
+    # Subtract half the interval to get to the bin borders from the central
+    # values
+    binning = vals - diffs / 2
+    # Now have to use the full interval for the last bin
+    binning = np.append(binning, np.array([binning[-1]] + diffs[-1]))
+
+    if get_vals:
+        return binning, vals
+    else:
+        return binning
+
---
+++ python/ptm_dep_band_graphs.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+"""
+Script to make graphs as a function of ptm for differerent variables from a ptm
+scan produced by run_ptM_scan
+"""
+import sys
+import pandas as pd
+import numpy as np
+
+from collections import OrderedDict
+
+import ROOT as r
+r.PyConfig.IgnoreCommandLineOptions = True
+
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s - %(funcName)s: %(message)s')
+
+from utils.data_handling import get_dataframe
+from utils.misc_helpers import create_random_str, quantile
+from utils.hist_utils import hist1d
+from utils.plot_helpers import get_x_min, get_x_max
+
+from common_helpers import (
+    get_var_name, get_variable_binning, frac_to_lam, identity
+)
+
+
+def process_input_variables(var_str):
+    """Process the input variables into a usable format"""
+    var_funcs = OrderedDict()
+    for vstr in var_str.split(','):
+        tmp = vstr.split(':')
+        if len(tmp) == 2:
+            func, var = tmp
+            if not func in globals():
+                logging.fatal('%s is not a valid (i.e. defined) transformation',
+                              func)
+                sys.exit(64)
+            func = globals()[func]
+        else:
+            var = tmp[0]
+            func = identity
+
+        var_funcs[var] = func
+
+    return var_funcs
+
+
+def get_quantile_values(var_name, trans_f, quants=[0.16, 0.5, 0.84]):
+    """Get the function that takes a dataframe as single argument and returns the
+    desired quantiles of the variable after applying the passed
+    transformation"""
+    return lambda d: quantile(trans_f(d.loc[:, var_name].values), quants)
+
+
+
+def make_graph(ptm_group_by, ptm_vals, var_name, trans_f):
+    """Make a graph of the passed variable"""
+    quantiles = ptm_group_by.apply(get_quantile_values(var_name, trans_f))
+
+    central = np.ones_like(ptm_vals)
+    low = np.zeros_like(ptm_vals)
+    high = np.zeros_like(ptm_vals)
+
+    for ipt in xrange(len(ptm_vals)):
+        central[ipt] = quantiles.iloc[ipt][1] # quantiles is a pd.Series!
+        low[ipt], high[ipt] = np.diff(quantiles.iloc[ipt])
+
+    return r.TGraphAsymmErrors(len(ptm_vals), ptm_vals, central,
+                               np.zeros_like(ptm_vals), np.zeros_like(ptm_vals),
+                               low, high)
+
+
+
+def main(args):
+    """Main"""
+    data = get_dataframe(args.scanfile)
+
+    # Get a "helper" ptm binning, that is only used in the groupby to easily
+    # identify the different ptM values
+    ptm_binning, ptm_vals = get_variable_binning(data.ptM, get_vals=True)
+    ptm_bins = data.groupby(pd.cut(data.ptM, ptm_binning))
+
+    outfile = r.TFile.Open(args.outfile, 'recreate')
+
+    for var, trans in args.variables.iteritems():
+        graph = make_graph(ptm_bins, ptm_vals,  var, trans)
+        graph.SetName('_'.join([get_var_name(var, trans.__name__), 'v', 'ptm']))
+        graph.Write()
+
+
+    outfile.Close()
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Script to make ptm dependent '
+                                     'graphs for different variables from a ptm '
+                                     'scan')
+    parser.add_argument('scanfile', help='File containing the scan results from '
+                        ' a run of run_ptM_scan')
+    parser.add_argument('-o', '--outfile', help='File to which the TGraph '
+                        'contour will be stored', default='ptm_dep_graphs.root')
+    parser.add_argument('-v', '--variables', help='Comma separated list of '
+                        'variables for which graphs should be produced. If a '
+                        'transformation should be applied to the variable this '
+                        'can be indicated by separating the transformation and '
+                        'the variable with a colon: TRANSFORMATION:VARIABLE',
+                        default='frac_to_lam:f_long_c1,frac_to_lam:f_long_c2',
+                        type=process_input_variables)
+
+    clargs = parser.parse_args()
+    main(clargs)
