499abb30205ea779b6054050d419ec39dd50a7e8
thomas madlener <thomas.madlener@cern.ch>
2020-04-25 20:41:28 +0200

    Run script to launch batch jobs for 2D correlation scan

---
+++ scripts/batch_correlation_scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J CorrelationScan
+#SBATCH -D /afs/hephy.at/work/t/tmadlener/chic_pol_global_fit/
+#SBATCH -o /afs/hephy.at/work/t/tmadlener/chic_pol_global_fit/batch_logfiles/run_correlation_scan_%A_%a.out
+
+source ${CHIB_CHIC_POLFW_DIR}/scripts/bash_functions.sh
+
+set -x
+
+## input arguments
+scanfile=${1}
+fitresult=${2}
+nscans=${3}
+
+set +x
+
+exe="/afs/hephy.at/work/t/tmadlener/chic_pol_global_fit/bin/run_pTM_scan"
+
+outdir=$(dirname ${scanfile})
+
+run_sandboxed ${outdir} ${exe} --outfile $(basename ${scanfile}) --npoints 1 --ptmmin 3 --noparams true --fitresult ${fitresult} --nscans ${nscans}
